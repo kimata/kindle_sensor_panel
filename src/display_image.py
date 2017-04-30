8ebb51381977d05619e820503d19a4da0ae3a8a2
KIMATA Tetsuya <kimata@green-rabbit.net>
2017-04-30 17:25:38 +0900

    backup.

--- src/display_image.py
+++ src/display_image.py
@@ -5,16 +5,19 @@ import paramiko
 import datetime
 import subprocess
 import time
+import os
 import gc
 
-KINDLE_IP   = '192.168.2.193'
+KINDLE_IP   = 'display-living'
 UPDATE_SEC  = 60
 REFRESH     = 60
+
+CREATE_IMAGE = os.path.dirname(os.path.abspath(__file__)) + 'create_image.py'
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(KINDLE_IP, username='root', password='mario', allow_agent=False, look_for_keys=False)
-
+print('connect ok.')
 ssh.exec_command('initctl stop powerd')
 ssh.exec_command('initctl stop framework')
 
@@ -25,7 +28,7 @@ while True:
       '-f' if (i % REFRESH) == 0 else ''
     ),
   )[0]
-  proc = subprocess.Popen(['python' , 'create_image.py'], stdout=subprocess.PIPE)
+  proc = subprocess.Popen(['python' , CREATE_IMAGE], stdout=subprocess.PIPE)
   ssh_stdin.write(proc.communicate()[0])
   ssh_stdin.close()
 
@@ -33,7 +36,10 @@ while True:
   del ssh_stdin
   gc.collect()
 
-  # 0 秒になるまで待ったうえで，指定時間待つ
+  # 更新されていることが直感的に理解しやすくなるように，更新タイミングを 0 秒
+  # に合わせる
+  # (例えば，1分間核更新だとして，1分59秒に更新されると，2分59秒まで更新されないので
+  # 2分40秒くらいに表示を見た人は本当に1分感覚で更新されているのか心配になる)
   time.sleep(UPDATE_SEC - datetime.datetime.now().second)
   
   i += 1
